b691a5c84186726f9ec4e28bb76dece6a7dafec7
patternplane <bss011202@naver.com>
2023-12-15 15:38:36 +0900

    [BAEK][RE] 백준 13023 문제 개선1
    - 잦은 조회를 하는 부분을 리스트 대신 배열로 변경
       -> 아주 작은 성능개선
    - 재귀 과정에서 기억하는 데이터의 구조 변경
       다음 탐색할 노드를 가리킬 때, 인덱스로 가리키도록 변경
       -> 탐색 성능 다소 개선
    - 스택 내 객체의 재활용
       -> 공간 효율성 증대, 성능 다소 개선

--- Algorithm_patternplane/src/Baek13023.java
+++ Algorithm_patternplane/src/Baek13023.java
@@ -4,55 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Person {
-    public List<Person> friends = new ArrayList<Person>();
+    private final List<Person> primitiveFriends = new ArrayList<Person>();
+    public Person[] friends;
     public boolean visited = false;
 
-    public Person getNextValidFriend(Person startPerson) {
+    public void addFriend(Person f) {
+        primitiveFriends.add(f);
+    }
 
-        int i = 0;
-
-        if (startPerson != null)  {
-            while (i < friends.size() && friends.get(i) != startPerson)
-                i++;
-            i++;
-        }
-
-        while (i < friends.size() && friends.get(i).visited)
-            i++;
-        if (i == friends.size())
-            return null;
-        else
-            return friends.get(i);
+    public void makeArray() {
+        friends = primitiveFriends.toArray(new Person[0]);
     }
 }
 
-class StackData {
+class StackData{
     public Person currentPerson;
-    public Person currentFriend;
-
-    public StackData(Person currentPerson, Person currentFriend) {
+    public int friendIdx;
+    public StackData(Person currentPerson, int friendIdx) {
         this.currentPerson = currentPerson;
-        this.currentFriend = currentFriend;
+        this.friendIdx = friendIdx;
     }
 }
 
 class Stack {
-    private List<StackData> stack = new ArrayList<>();
+    private final List<StackData> stack = new ArrayList<>();
     private int top = -1;
+
+    public void clear() {
+        top = -1;
+    }
 
     public boolean isEmpty() {
         return (top == -1);
     }
 
-    public void push(StackData data) {
-        stack.add(data);
+    public void push(Person currentPerson, int friendIdx) {
         top++;
+        if (top >= stack.size())
+            stack.add(new StackData(currentPerson, friendIdx));
+        else {
+            stack.get(top).currentPerson = currentPerson;
+            stack.get(top).friendIdx = friendIdx;
+        }
     }
 
     public StackData pop() {
-        StackData result = stack.get(top);
-        stack.remove(top--);
-        return result;
+        return stack.get(top--);
     }
 
     public StackData peak() {
@@ -60,9 +57,8 @@ class Stack {
     }
 }
 
-public class Baek13023 {
-
-    private Person[] getInput() {
+class Inputter{
+    public static Person[] getInput() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Person[] p;
@@ -79,9 +75,12 @@ public class Baek13023 {
                 input = br.readLine().split(" ");
                 int person1 = Integer.parseInt(input[0]);
                 int person2 = Integer.parseInt(input[1]);
-                p[person1].friends.add(p[person2]);
-                p[person2].friends.add(p[person1]);
+                p[person1].addFriend(p[person2]);
+                p[person2].addFriend(p[person1]);
             }
+
+            for (int i = 0; i < peopleLen; i++)
+                p[i].makeArray();
 
             return p;
 
@@ -90,58 +89,68 @@ public class Baek13023 {
             return null;
         }
     }
+}
 
-    private int FindFriendSize(Person[] people, int findIdx) {
+class DFSFriendLineTracer{
 
-        Stack stack = new Stack();
-        int maxVisitedCnt = 0;
+    private static final Stack stack = new Stack();
+
+    public static boolean findFriendLine(Person[] people, int startPerson, int friendDistance) {
 
         // Init
-        int visitedCnt = 0;
-        if (!people[findIdx].friends.isEmpty()) {
-            stack.push(new StackData(people[findIdx], people[findIdx].friends.get(0)));
-            stack.peak().currentPerson.visited = true;
-            visitedCnt++;
-            maxVisitedCnt = visitedCnt;
-        }
+        stack.clear();
+        people[startPerson].visited = true;
+        int friendDis = 1;
+        stack.push(people[startPerson], -1);
 
         // DFS
         while (!stack.isEmpty()) {
-            if (stack.peak().currentFriend != null) {
-                StackData nextPersonData = new StackData(
-                        stack.peak().currentFriend,
-                        stack.peak().currentFriend.getNextValidFriend(null)
-                );
-                nextPersonData.currentPerson.visited = true;
-                stack.peak().currentFriend = stack.peak().currentPerson.getNextValidFriend(stack.peak().currentFriend);
-                stack.push(nextPersonData);
-                visitedCnt++;
 
-                if (maxVisitedCnt < visitedCnt) {
-                    maxVisitedCnt = visitedCnt;
-                    if (maxVisitedCnt == 5)
-                        return maxVisitedCnt;
+            Person currentPerson = stack.peak().currentPerson;
+
+            // get next friend
+            int nextFriendIdx = -1;
+            for (int i = stack.peak().friendIdx + 1; i < currentPerson.friends.length; i++)
+                if (!currentPerson.friends[i].visited) {
+                    nextFriendIdx = i;
+                    break;
                 }
+
+            // backtrack or keep going
+            if (nextFriendIdx == -1) {
+                currentPerson.visited = false;
+                friendDis--;
+                stack.pop();
             }
             else {
-                stack.peak().currentPerson.visited = false;
-                stack.pop();
-                visitedCnt--;
+                currentPerson.friends[nextFriendIdx].visited = true;
+                friendDis++;
+                if (friendDis == friendDistance)
+                    return true;
+
+                stack.peak().friendIdx = nextFriendIdx;
+                stack.push(currentPerson.friends[nextFriendIdx], -1);
             }
         }
 
-        return maxVisitedCnt;
+        return false;
     }
+}
+
+public class Baek13023 {
 
     public void Solution() {
-        Person[] people = getInput();
+        Person[] people = Inputter.getInput();
         if (people == null)
             return;
 
-        int maxVisitedCnt = 0;
-        for (int peopleIdx = 0; peopleIdx < people.length && maxVisitedCnt < 5; peopleIdx++)
-            maxVisitedCnt = FindFriendSize(people, peopleIdx);
+        boolean find = false;
+        for (int personIdx = 0; personIdx < people.length; personIdx++) {
+            find = DFSFriendLineTracer.findFriendLine(people, personIdx, 5);
+            if (find)
+                break;
+        }
 
-        System.out.print((maxVisitedCnt == 5 ? 1 : 0));
+        System.out.print(find ? 1 : 0);
     }
 }
