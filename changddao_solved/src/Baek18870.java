9d1f69759d406e71b64e11633037b6a968aa5f3f
Changho Youn <younch8342@yu.ac.kr>
2023-12-15 16:55:21 +0900

    HashSet 또는 HashMap을 이용한 문제풀이(근데 Set사용할 때 시간초과 뜨는데 이건 좀 너무한듯)

---
+++ changddao_solved/src/Baek18870.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Baek18870 {
+    //Set 사용하기
+   /* static int n;
+    static int[] srcNums;
+    static int[] sortedNums;*/
+   /* public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Set<Integer> hashSet = new HashSet<>();
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            nums[i] = num;
+            hashSet.add(num);
+        }// nums 배열 초기화
+                                        //java 17에서는 sorted 다음 바로 toList() 가능
+                                        //but, 아래 버전 .collect 써야함.
+        List<Integer> sortedArr = hashSet.stream().sorted().collect(Collectors.toList());
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            int compareTarget = nums[i];
+            for (int c : sortedArr) {
+                if (compareTarget > c)
+                    cnt++;
+                else break;
+            }
+            sb.append(cnt).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }*/
+    static int n;
+    static int[] srcNums;
+    static int[] sortedNums;
+    //HashMap 이용하여 순위 매기기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Map<Integer, Integer> map = new HashMap<>();
+        n = Integer.parseInt(br.readLine());
+        srcNums = new int[n];
+        sortedNums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            srcNums[i] = num;
+            sortedNums[i] = num;
+        }
+        Arrays.sort(sortedNums);
+        int rank = 0;
+        for (int num : sortedNums) {
+            if (!map.containsKey(num)) {
+                map.put(num, rank);
+                rank++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            sb.append(map.get(srcNums[i])).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+}
