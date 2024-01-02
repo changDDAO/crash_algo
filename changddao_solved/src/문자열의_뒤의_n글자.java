a4b5e5f07f93ff9e5cc4ac91a1b376037198b5fb
Changho Youn <younch8342@yu.ac.kr>
2024-01-02 15:28:25 +0900

    프로그래머스_기초문제도 다지기

---
+++ changddao_solved/src/문자열의_뒤의_n글자.java
@@ -0,0 +1,9 @@
+public class 문자열의_뒤의_n글자 {
+    class Solution {
+        public String solution(String my_string, int n) {
+            String answer = my_string;
+            answer = answer.substring(answer.length()-n);
+            return answer;
+        }
+    }
+}
