2adb8412830c128a8c298bd489a0b47665727bcc
changDDAO <younch8342@naver.com>
2024-01-05 18:36:09 +0900

    문자열 문제라고 칭해져 있지만, 그리디에 가까운 하지만 풀이하는데 2시간 소요된 문제(IOI)
    Signed-off-by: Changho Youn <younch8342@yu.ac.kr>

---
+++ changddao_solved/src/Baek5525.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek5525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        int cnt = 0;
+        int answer = 0;
+        //그리디 잘못 걸리면 박살남...
+        for (int i = 1; i < m - 1; i++) {
+            if (s.charAt(i - 1) == 'I' && s.charAt(i) == 'O' && s.charAt(i + 1) == 'I') {
+                cnt++;
+                if(cnt==n){
+                    cnt--;
+                    answer++;
+                }
+                i++;
+            }else{
+                cnt=0;
+            }
+        }
+        System.out.println(answer);
+    }
+}
