ad5f0f94b288df4c4be1f6586664335b77a72907
Changho Youn <younch8342@yu.ac.kr>
2024-01-05 15:53:42 +0900

    PriorityQueue를 사용하여 문제 풀이

---
+++ changddao_solved/src/더맵게.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+
+public class 더맵게 {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int sco : scoville){
+            pq.offer(sco);
+        }
+        while(pq.peek()<K){
+            if(pq.size()==1){
+                return -1;
+            }
+            int least = pq.poll();
+            int second = pq.poll();
+            int mixed = least+ second*2;
+            pq.offer(mixed);
+            answer++;
+        }
+
+        return answer;
+    }
+}
