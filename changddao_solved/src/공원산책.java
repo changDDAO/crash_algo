2f50929d3809cb02aba8cde9284b96be35c9959f
changDDAO <younch8342@naver.com>
2024-01-09 11:35:59 +0900

    [프로그래머스] 공원산책 구현문제 풀이

---
+++ changddao_solved/src/공원산책.java
@@ -0,0 +1,44 @@
+public class 공원산책 {
+    public int[] solution(String[] park, String[] routes) {
+        int x = park.length;
+        int y = park[0].length();
+        int [] startPos={0,0};
+        //동서남북 정의하기
+        char [][]map = new char[x][y];
+        for(int i = 0 ; i< park.length;i++){
+            map[i] = park[i].toCharArray();
+            if(park[i].contains("S")){
+                startPos[0]=i;
+                startPos[1]=park[i].indexOf("S");
+            }
+        }
+        for(String route: routes){
+            String []temp = route.split(" ");
+            char dir = temp[0].charAt(0);
+            int size = Integer.parseInt(temp[1]);
+            int nx = startPos[0];
+            int ny = startPos[1];
+            for(int i=0; i<size;i++){
+                if(dir=='E')
+                    ny++;
+                if(dir=='W')
+                    ny--;
+                if(dir=='S')
+                    nx++;
+                if(dir=='N')
+                    nx--;
+                if(nx<0||nx>=x||ny<0||ny>=y) break;이
+                if(map[nx][ny]=='X') break;
+                if(i== size-1){
+                    startPos[0]=nx;
+                    startPos[1]=ny;
+
+                }
+            }
+        }
+
+
+        int []answer = {startPos[0],startPos[1]};
+        return answer;
+    }
+}
