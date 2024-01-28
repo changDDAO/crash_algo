321d42551f05d658152cd442bf12ba2343b1e648
changDDAO <younch8342@naver.com>
2024-01-28 20:34:34 +0900

    [프로그래머스] 문제풀기

---
+++ changddao_solved/src/programmers/마지막_두원소.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class 마지막_두원소 {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        int n = num_list.length;
+        answer = new int[n+1];
+
+        int prev = num_list[n-2];
+        int end = num_list[n-1];
+        for(int i=0; i<n; i++){
+            answer[i] = num_list[i];
+        }
+        if(prev<end){
+            answer[n]=end-prev;
+        }else{
+            answer[n]=end*2;
+        }
+        return answer;
+    }
+}
