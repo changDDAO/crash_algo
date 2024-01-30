74e74612daa104aeae5dd5a6d8a15d694404b94e
changDDAO <younch8342@naver.com>
2024-01-30 18:39:29 +0900

    [프로그래머스] 조이스틱 주석달기

--- changddao_solved/src/programmers/조이스틱.java
+++ changddao_solved/src/programmers/조이스틱.java
@@ -6,14 +6,16 @@ public class 조이스틱 {
         //오른쪽으로 쭉 간 횟수
         int move  = name.length()-1;
         //상,하 비교하기
-        for(int i =0; i<name.length();i++){
+        for(int i =0; i<name.length();i++){  //CCCCAAAAAAAAAABCB 17 -14
             answer+=Math.min(name.charAt(i)-'A', 26 - (name.charAt(i)-'A'));
             if(i<name.length()-1 && name.charAt(i+1)=='A'){
                 int endA = i+1;
                 while(endA< name.length() && name.charAt(endA) =='A')
                     endA++;
-                move = Math.min(move, i*2 + name.length()-endA);
+                move = Math.min(move, i*2 + name.length()-endA);//오른쪽으로 시작해서 움직이다가
+                //다시 돌아와서 뒤로 움직인횟수를 count
                 move = Math.min(move, (name.length()-endA)*2 + i);
+                // 뒤로움직였다가 시작지점으로 돌아와서 오른쪽 count해나가기
             }
         }
 
