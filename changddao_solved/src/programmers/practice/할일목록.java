76d58562ae46d5e151e2258144b562709176d540
changDDAO <younch8342@naver.com>
2024-02-11 23:44:28 +0900

    [프로그래머스] 할일목록

---
+++ changddao_solved/src/programmers/practice/할일목록.java
@@ -0,0 +1,17 @@
+package programmers.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 할일목록 {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> doList = new ArrayList<>();
+        for(int i =0; i<finished.length; i++){
+            if(!finished[i]){
+                doList.add(todo_list[i]);
+            }
+        }
+        String[] answer = doList.stream().toArray(String[]::new);
+        return answer;
+    }
+}
