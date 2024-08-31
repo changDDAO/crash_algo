1ba4223e99aead0de71b8340773f63c0620ec446
changDDAO <younch8342@naver.com>
2024-08-31 13:20:48 +0900

    프로그래머스 세로읽기 coding practice

---
+++ changddao_solved/src/programmers/practice/세로읽기.java
@@ -0,0 +1,20 @@
+package programmers.practice;
+
+import java.util.*;
+
+class 세로읽기 {
+    static List<String> strList = new ArrayList<>();
+    public String solution(String my_string, int m, int c) {
+        StringBuilder sb = new StringBuilder();
+        String answer = "";
+        for(int i = 0; i<my_string.length(); i+=m){
+            String prev= my_string.substring(i, i+m);
+            strList.add(prev);
+        }
+        for(int i=0; i<strList.size();i++){
+            sb.append(strList.get(i).charAt(c-1));
+        }
+        answer= sb.toString();
+        return answer;
+    }
+}
