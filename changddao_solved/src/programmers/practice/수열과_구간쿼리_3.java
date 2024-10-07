c1edc957eac520f32facfe3383eac1088c8c51ae
changDDAO <younch8342@naver.com>
2024-10-08 00:23:03 +0900

    수열과 구간쿼리 자바

---
+++ changddao_solved/src/programmers/practice/수열과_구간쿼리_3.java
@@ -0,0 +1,15 @@
+package programmers.practice;
+
+public class 수열과_구간쿼리_3 {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i = 0; i<queries.length; i++){
+            int left = queries[i][0];
+            int right = queries[i][1];
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+
+        return arr;
+    }
+}
