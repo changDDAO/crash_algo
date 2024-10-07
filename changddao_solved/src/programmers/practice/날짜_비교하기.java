856a7085668e0e942a54f5460e68aef5b0de8a2e
changDDAO <younch8342@naver.com>
2024-10-08 00:03:42 +0900

    날짜_비교하기 자바

---
+++ changddao_solved/src/programmers/practice/날짜_비교하기.java
@@ -0,0 +1,18 @@
+package programmers.practice;
+
+public class 날짜_비교하기 {
+    public int solution(int[] date1, int[] date2) {
+        int answer = 0;
+        for(int i =0; i< date1.length; i++){
+            if(date1[i]>date2[i]){
+                answer=0;
+                break;
+            }
+            if(date1[i]<date2[i]){
+                answer=1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
