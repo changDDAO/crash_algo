32358d9b7d1630bfacbbb38cf553fb343a4b8ca3
changDDAO <younch8342@naver.com>
2024-10-20 01:34:43 +0900

    프로그래머스 문자열 묶기 문제풀이

---
+++ changddao_solved/src/programmers/practice/문자열_묶기.java
@@ -0,0 +1,27 @@
+package programmers.practice;
+
+public class 문자열_묶기 {
+    public int solution(String[] strArr) {
+        int answer = 0;
+        int len = 0;
+        for(String temp : strArr){
+            if(temp.length()>len){
+                len = temp.length();
+            }
+        }
+        for(int i = 1; i<=len; i++){
+            int cnt = 0;
+            for(int j = 0; j<strArr.length; j++){
+                if(strArr[j].length()==i){
+                    cnt++;
+                }
+            }
+            if(cnt>answer){
+                answer=cnt;
+            }
+        }
+
+
+        return answer;
+    }
+}
