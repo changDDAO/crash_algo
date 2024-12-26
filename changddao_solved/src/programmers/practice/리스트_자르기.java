bc8c85afe271e67cdc5b9800ce388ed157d919c3
changDDAO <younch8342@naver.com>
2024-12-26 23:59:14 +0900

    프로그래머스 리스트자르기 문제풀이

---
+++ changddao_solved/src/programmers/practice/리스트_자르기.java
@@ -0,0 +1,8 @@
+package programmers.practice;
+
+public class 리스트_자르기 {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int[] answer = {};
+        return answer;
+    }
+}
