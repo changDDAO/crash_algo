c9779dd99d8f1a0536b45ff73a2692ab8be3ae9c
changDDAO <younch8342@naver.com>
2025-02-16 14:48:06 +0900

    feat: 수나누기 문제풀이

---
+++ changddao_solved/src/programmers/lv1/수나누기.java
@@ -0,0 +1,18 @@
+package programmers.lv1;
+
+import java.util.Scanner;
+
+public class 수나누기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int answer = 0;
+
+        while(number!=0){
+            answer += number % 100;
+            number /= 100;
+        }
+
+        System.out.println(answer);
+    }
+}
