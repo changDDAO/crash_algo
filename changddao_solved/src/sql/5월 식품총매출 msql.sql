449ec7e1c0620e697417e4f654f0ff261bbf4de3
changDDAO <younch8342@naver.com>
2025-04-11 14:20:34 +0900

    year, month 를 이용한 filtering도 가능

--- changddao_solved/src/sql/5월 식품총매출 msql.sql
+++ changddao_solved/src/sql/5월 식품총매출 msql.sql
@@ -3,3 +3,11 @@ from food_product a join food_order b on (a.product_id = b.product_id)
 where b.produce_date like '2022-05%'
 group by a.product_id
 order by total_sales desc, a.product_id;
+
+/*select a.product_id, a.product_name, sum(amount*price) as total_sales
+from food_product a join food_order b on (a.product_id = b.product_id)
+where year(b.produce_date) = 2022
+and month(b.produce_date) = 5
+group by a.product_id
+order by total_sales desc, a.product_id;
+*/
