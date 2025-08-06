598b2d1dd12c45a8f21f9d91d4faa16dbcd5b848
changDDAO <younch8342@naver.com>
2025-08-06 17:42:19 +0900

    feat: 문제에서 주어진 조건을 활용하여 Stirng 형식을 second로 통일하여 계산 후, 형식 맞춰주기

---
+++ changddao_solved/src/프로그래머스/PCCP1.java
@@ -0,0 +1,39 @@
+package 프로그래머스;
+
+public class PCCP1 {
+
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        int videoLen = toSeconds(video_len);
+        int current = toSeconds(pos);
+        int opStart = toSeconds(op_start);
+        int opEnd = toSeconds(op_end);
+
+        for (String cmd : commands) {
+            if (cmd.equals("prev")) {
+                current = Math.max(0, current - 10);
+            } else if (cmd.equals("next")) {
+                current = Math.min(videoLen, current + 10);
+            }
+
+
+            if (current >= opStart && current <= opEnd) {
+                current = opEnd;
+            }
+        }
+
+        return toMs(current);
+    }
+
+    private int toSeconds(String time) {
+        String[] temp = time.split(":");
+        return Integer.parseInt(temp[0]) * 60 + Integer.parseInt(temp[1]);
+    }
+
+    private String toMs(int seconds) {
+        int minute = seconds / 60;
+        int second = seconds % 60;
+        return String.format("%02d:%02d", minute, second);
+    }
+}
+
+
