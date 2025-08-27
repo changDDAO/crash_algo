ecbdaee1d243a0b38e7a27b2a25e8f5adcba2b8c
changDDAO <younch8342@naver.com>
2025-08-27 12:56:49 +0900

    feat: int 형의 자릿 수를 나누기와 나머지를 이용한 연산

--- changddao_solved/src/백준/Baek1110.java
+++ changddao_solved/src/백준/Baek1110.java
@@ -6,6 +6,18 @@ public class Baek1110 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println("n= "+ n);
+        sc.close();
+        int cnt = 0;
+        int newNum = n;
+        while (true) {
+            cnt++;
+            int sum = newNum/10 + newNum%10;
+            newNum = (newNum%10) * 10  + (sum % 10);
+            if(newNum == n){
+                break;
+            }
+        }
+        System.out.println(cnt);
+        
     }
 }
