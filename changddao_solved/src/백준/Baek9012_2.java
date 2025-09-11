a1e86a05c4617b9a8bd459543f1dfb564f3f444c
changDDAO <younch8342@naver.com>
2025-09-11 20:04:15 +0900

    feat: Stack 원리를 이용하여 구현할 수도있고, 단순입력값을 찾아가면서 NPS찾기

--- changddao_solved/src/백준/Baek9012_2.java
+++ changddao_solved/src/백준/Baek9012_2.java
@@ -1,11 +1,37 @@
 package 백준;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baek9012_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            int left=0 , right =0;
+            for (int j = 0; j < input.length(); j++) {
+                char temp = input.charAt(j);
+                if(temp == '('){
+                    left++;
+                }else{
+                    right++;
+                }
+                if (right > left) {
+                    break;
+                }
+            }
+            if (left == right) {
+                sb.append("YES").append("\n");
+            }else{
+                sb.append("NO").append("\n");
+            }
+        }
+        System.out.println(sb);
+
+
         
     }
 }
